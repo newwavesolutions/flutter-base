e62c01b6f865c36761d1afcbaaf67c8ee27dc3d7
ThangND <thangnd@newwave.com.vn>
2023-05-12 14:09:28 +0700

    feat: update DateTime dialog

--- lib/ui/commons/app_dialog.dart
+++ lib/ui/commons/app_dialog.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base/configs/app_configs.dart';
 import 'package:get/get.dart';
 
 class AppDialog {
+  static final _now = DateTime.now();
+  static final _firstDateDefault =
+      DateTime(_now.year - 100, _now.month, _now.day);
+  static final _lastDateDefault =
+      DateTime(_now.year + 100, _now.month, _now.day);
+  static final _timeNow = TimeOfDay(hour: _now.hour, minute: _now.minute);
+
   static void defaultDialog({
     String title = "Alert",
     String message = "",
@@ -33,39 +41,33 @@ class AppDialog {
     );
   }
 
-// static void showDatePicker(
-//   BuildContext context, {
-//   DateTime? minTime,
-//   DateTime? maxTime,
-//   DateChangedCallback? onConfirm,
-//   LocaleType locale = LocaleType.en,
-//   DateTime? currentTime,
-// }) {
-//   DatePicker.showDatePicker(
-//     context,
-//     minTime: minTime,
-//     maxTime: maxTime,
-//     onConfirm: onConfirm,
-//     locale: LocaleType.vi,
-//     currentTime: currentTime,
-//   );
-// }
-//
-// static void showDateTimePicker(
-//   BuildContext context, {
-//   DateTime? minTime,
-//   DateTime? maxTime,
-//   DateChangedCallback? onConfirm,
-//   LocaleType locale = LocaleType.en,
-//   DateTime? currentTime,
-// }) {
-//   DatePicker.showDateTimePicker(
-//     context,
-//     minTime: minTime,
-//     maxTime: maxTime,
-//     onConfirm: onConfirm,
-//     locale: LocaleType.vi,
-//     currentTime: currentTime,
-//   );
-// }
+  static Future<DateTime?> showDateDialog(
+    BuildContext context, {
+    DateTime? minDate,
+    DateTime? maxDate,
+    DateTime? currentDate,
+    Locale? locale = AppConfigs.defaultLocal,
+  }) {
+    final firstDate = minDate ?? _firstDateDefault;
+    final lastDate = maxDate ?? _lastDateDefault;
+    return showDatePicker(
+      context: context,
+      initialDate: _now,
+      firstDate: firstDate,
+      lastDate: lastDate,
+      currentDate: currentDate,
+      locale: locale,
+    );
+  }
+
+  static Future<TimeOfDay?> showTimeDialog(
+    BuildContext context, {
+    TimeOfDay? initialTime,
+  }) {
+    final initial = initialTime ?? _timeNow;
+    return showTimePicker(
+      context: context,
+      initialTime: initial,
+    );
+  }
 }
