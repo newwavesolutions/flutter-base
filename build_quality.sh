eb9afcfd4b154e31e3650181e2a619fd999fe4cc
thoson-nws <sonlt2@newwave.com.vn>
2023-05-15 22:50:57 +0700

    feat: update code quality format

--- build_quality.sh
+++ build_quality.sh
@@ -2,5 +2,5 @@ flutter clean
 flutter pub get
 flutter pub run intl_utils:generate
 flutter pub run build_runner build --delete-conflicting-outputs
-flutter format ./lib --set-exit-if-changed
+dart format ./lib --set-exit-if-changed
 flutter analyze
