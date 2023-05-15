809853e9fa35f71f66d7cb9181fcbd57fa4f0565
thoson-nws <sonlt2@newwave.com.vn>
2023-05-15 21:59:17 +0700

    feat: upgrade flutter 3.10.0

--- lib/ui/commons/app_dialog.dart
+++ lib/ui/commons/app_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:get/get.dart';
 
 class AppDialog {
@@ -34,41 +33,39 @@ class AppDialog {
     );
   }
 
-  static void showDatePicker(
-    BuildContext context, {
-    DateTime? minTime,
-    DateTime? maxTime,
-    DateChangedCallback? onConfirm,
-    LocaleType locale = LocaleType.en,
-    DateTime? currentTime,
-  }) {
-    DatePicker.showDatePicker(
-      context,
-      minTime: minTime,
-      maxTime: maxTime,
-      onConfirm: onConfirm,
-      locale: LocaleType.vi,
-      currentTime: currentTime,
-      theme: const DatePickerTheme(),
-    );
-  }
-
-  static void showDateTimePicker(
-    BuildContext context, {
-    DateTime? minTime,
-    DateTime? maxTime,
-    DateChangedCallback? onConfirm,
-    LocaleType locale = LocaleType.en,
-    DateTime? currentTime,
-  }) {
-    DatePicker.showDateTimePicker(
-      context,
-      minTime: minTime,
-      maxTime: maxTime,
-      onConfirm: onConfirm,
-      locale: LocaleType.vi,
-      currentTime: currentTime,
-      theme: const DatePickerTheme(),
-    );
-  }
+// static void showDatePicker(
+//   BuildContext context, {
+//   DateTime? minTime,
+//   DateTime? maxTime,
+//   DateChangedCallback? onConfirm,
+//   LocaleType locale = LocaleType.en,
+//   DateTime? currentTime,
+// }) {
+//   DatePicker.showDatePicker(
+//     context,
+//     minTime: minTime,
+//     maxTime: maxTime,
+//     onConfirm: onConfirm,
+//     locale: LocaleType.vi,
+//     currentTime: currentTime,
+//   );
+// }
+//
+// static void showDateTimePicker(
+//   BuildContext context, {
+//   DateTime? minTime,
+//   DateTime? maxTime,
+//   DateChangedCallback? onConfirm,
+//   LocaleType locale = LocaleType.en,
+//   DateTime? currentTime,
+// }) {
+//   DatePicker.showDateTimePicker(
+//     context,
+//     minTime: minTime,
+//     maxTime: maxTime,
+//     onConfirm: onConfirm,
+//     locale: LocaleType.vi,
+//     currentTime: currentTime,
+//   );
+// }
 }
