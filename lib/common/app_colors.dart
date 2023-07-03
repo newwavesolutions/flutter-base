bd8c56910f6087ec1971b8659bbfd646505de305
thoson-nws <sonlt2@newwave.com.vn>
2023-07-03 10:36:36 +0700

    feat: update splash screen

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   AppColors._();
 
   ///Common
-  static const Color primary = Color(0xFFD9251D);
+  static const Color primary = Color(0xFF173d7e);
   static const Color secondary = Color(0xFF47CFFF);
 
   ///Background
--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base/blocs/app_cubit.dart';
+import 'package:flutter_base/common/app_colors.dart';
 import 'package:flutter_base/common/app_images.dart';
 import 'package:flutter_base/repositories/auth_repository.dart';
 import 'package:flutter_base/ui/pages/splash/splash_navigator.dart';
@@ -46,11 +47,11 @@ class _SplashChildPageState extends State<SplashChildPage> {
     return Scaffold(
       body: Stack(
         children: [
+          Positioned.fill(child: Container(color: AppColors.primary)),
           Center(
-            child: SizedBox(
-              height: 200,
+            child: Image.asset(
+              AppImages.icLogoTransparent,
               width: 200,
-              child: Image.asset(AppImages.icLogoTransparent),
             ),
           ),
         ],
