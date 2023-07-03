6815e18a424d26988f8bfce9bf300286712ebff1
hungdq <hungdq@newwave.vn>
2023-07-03 16:01:59 +0700

    add logic refresh token

--- lib/network/api_interceptors.dart
+++ lib/network/api_interceptors.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_base/database/secure_storage_helper.dart';
+import 'package:flutter_base/network/api_util.dart';
 import 'package:flutter_base/router/route_config.dart';
 import 'package:flutter_base/utils/logger.dart';
 import 'package:go_router/go_router.dart';
@@ -42,25 +44,92 @@ class ApiInterceptors extends QueuedInterceptorsWrapper {
     final data = jsonEncode(response.data);
     logger.log("✅ RESPONSE[$statusCode] => PATH: $uri\n DATA: $data");
     //Handle section expired
-    if (response.statusCode == 401) {
-      SecureStorageHelper.instance.removeToken();
-      _forceSignIn();
-    }
+    // if (response.statusCode == 401) {
+    //   SecureStorageHelper.instance.removeToken();
+    //   _forceSignIn();
+    // }
     super.onResponse(response, handler);
   }
 
   @override
-  void onError(DioError err, ErrorInterceptorHandler handler) {
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
     final statusCode = err.response?.statusCode;
-    final uri = err.requestOptions.path;
-    var data = "";
-    try {
-      data = jsonEncode(err.response?.data);
-    } catch (e, s) {
-      logger.e(e, stackTrace: s);
+    final path = err.requestOptions.path;
+    final uri = err.requestOptions.uri;
+    final RequestOptions request = err.requestOptions;
+    if (kDebugMode) {
+      logger.e(
+          "⚠️ ERROR[$statusCode] => PATH: $path \n Response: ${err.response?.data}");
     }
-    logger.log("⚠️ ERROR[$statusCode] => PATH: $uri\n DATA: $data");
-    super.onError(err, handler);
+    switch (statusCode) {
+      case 401:
+        final saveToken = await SecureStorageHelper.instance.getToken();
+        List<String> tokens =
+            err.requestOptions.headers['Authorization'].toString().split(' ');
+
+        String tokenRequest = tokens.length > 1 ? tokens[1] : "";
+        if (saveToken != null && saveToken.accessToken != tokenRequest) {
+          final cloneReq = await cloneRequest(
+            accessToken: saveToken.accessToken,
+            request: request,
+            uri: uri,
+          );
+          return handler.resolve(cloneReq);
+        }
+
+        if (saveToken == null) {
+          return handler.next(err);
+        }
+
+        try {
+          final result = await ApiUtil.onRefreshToken(
+            saveToken.refreshToken,
+          );
+
+          if (result != null) {
+            SecureStorageHelper.instance.saveToken(result);
+
+            final cloneReq = await cloneRequest(
+              accessToken: result.accessToken,
+              request: request,
+              uri: uri,
+            );
+            return handler.resolve(cloneReq);
+          } else {
+            if (kDebugMode) {
+              logger.e("Response refresh token is null");
+            }
+            _forceSignIn();
+            // return handler.next(err);
+          }
+        } catch (e) {
+          if (kDebugMode) {
+            logger.e(
+                "Api refresh token error $e, msg: ${(e as DioError).response}");
+          }
+          _forceSignIn();
+          // return handler.next(err);
+        }
+      default:
+        handler.next(err);
+    }
+  }
+
+  Future<Response> cloneRequest({
+    required String accessToken,
+    required RequestOptions request,
+    required Uri uri,
+  }) async {
+    final newOptions = Options(
+      method: request.method,
+      headers: request.headers,
+    );
+    newOptions.headers!['Authorization'] = 'Bearer $accessToken';
+    return await ApiUtil.getDio().requestUri(
+      uri,
+      options: newOptions,
+      data: request.data,
+    );
   }
 
   void _forceSignIn() {
--- lib/network/api_util.dart
+++ lib/network/api_util.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_base/configs/app_configs.dart';
+import 'package:flutter_base/models/entities/token_entity.dart';
+import 'package:flutter_base/models/response/object_response.dart';
+import 'package:flutter_base/utils/logger.dart';
 
 import 'api_client.dart';
 import 'api_interceptors.dart';
@@ -24,5 +27,35 @@ class ApiUtil {
   static ApiClient get mocKyApiClient {
     final apiClient = ApiClient(getDio(), baseUrl: AppConfigs.mocKyBaseUrl);
     return apiClient;
+  }
+
+  static Future<TokenEntity?> onRefreshToken(String? token) async {
+    if (token == null) return null;
+    final dio = Dio();
+    dio.options.connectTimeout = const Duration(milliseconds: 60000);
+    dio.options.headers['Authorization'] = "Bearer $token";
+    dio.options.headers['connection'] = "keep-alive";
+    final res = await dio.get('${AppConfigs.mocKyBaseUrl}/refresh-token');
+    logger.log(
+      "✈️ REQUEST[GET] => PATH: ${AppConfigs.mocKyBaseUrl}/refresh-token \n Token: ${dio.options.headers}",
+      printFullText: true,
+    );
+
+    final value = res.data == null
+        ? null
+        : ObjectResponse<TokenEntity>.fromJson(
+            res.data!,
+            (json) => TokenEntity.fromJson(json as Map<String, dynamic>),
+          );
+    if (value != null && value.data != null) {
+      logger.d(
+          '------------------------Get New AccessToken---------------------------\n');
+      logger.log(
+        'AccessToken token: ${value.data?.accessToken}',
+        printFullText: true,
+      );
+      return value.data;
+    }
+    return null;
   }
 }
